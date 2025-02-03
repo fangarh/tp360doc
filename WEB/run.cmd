2ca1fb36ec598d29987425e0b15e9a370c14ae8a
fangarh <57655184+fangarh@users.noreply.github.com>
2025-02-03 13:05:05 +0300

    ..

--- WEB/run.cmd
+++
@@ -1,6 +0,0 @@
-rmdir /Q /S output-folder
-rmdir /Q /S ~
-rmdir /Q /S ./../GitPub/WEB
-
-echo "yfm  -o ./../GitPub /build"
-npx -y @diplodoc/cli@next -o ./../GitPub/WEB --output-format html && npx -y @diplodoc/cli@next -o ~/output-docs --output-format html && npx http-server ~/output-docs -p 5005 
---
+++ docs/run.cmd
@@ -0,0 +1,6 @@
+rmdir /Q /S output-folder
+rmdir /Q /S ~
+rmdir /Q /S ./../GitPub/WEB
+
+echo "yfm  -o ./../GitPub /build"
+npx -y @diplodoc/cli@next -o ./../GitPub/docs --output-format html && npx -y @diplodoc/cli@next -o ~/output-docs --output-format html && npx http-server ~/output-docs -p 5005 
